7f40615622a53024da96823884135beb335da689
rodrigofontes15 <rodrigofontes15@users.noreply.github.com>
2020-02-01 17:25:44 +0100

    routing configured

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -16,5 +16,21 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
         }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/release/{year}/{month}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
+        
 }
